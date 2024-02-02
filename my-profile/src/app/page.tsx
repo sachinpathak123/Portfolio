931ca3568d8be586780d37a4b721e752481b4ac3
Sachin <pathaksachin876@gmail.com>
2024-02-02 15:07:33 +0530

    changes added

--- my-profile/src/app/page.tsx
+++ my-profile/src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import Side from './pages/Home/Side'
 
 const page = () => {
   return (
     <div>
       <Home />
+      <Side />
     </div>
   )
 }
---
+++ my-profile/src/app/pages/Home/Side.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Side = () => {
+  return (
+    <div>
+      <h6 className='rotate-90 text-left text-3xl space-x-5'>
+        FOLLOW FOR MORE
+      </h6>
+    </div>
+  )
+}
+
+export default Side
